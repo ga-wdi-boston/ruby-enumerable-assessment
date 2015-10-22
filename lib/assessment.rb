f85325dc3c5cb33c73d82400ba63f6ecbd5633bb
sarabrent <sarabrentmccoy@gmail.com>
2015-10-22 10:30:22 -0400

    completed assessment

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,36 +5,52 @@ class Assessment
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    enum.max {|a, b| a.length <=> b.length}
+    end
   end
+
+  puts longest
 
   # Question 2
   # Write ruby code to return the sum of entries in the
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+    enum.reduce{|a, b| a + b}
+    end
   end
+
+  puts sum
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.any? {|number| number%2 == 1}
+    end
   end
+
+  puts some_odd
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.all? {|number| number%2 == 0}
+    end
   end
+
+
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized.
+
+  #help
   def transform(words)
-    # Your code goes here
+    memo = 0
+    upcase.map {|memo, word| memo + word
+    end
   end
 
   # Question 6
@@ -43,6 +59,13 @@ class Assessment
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
+    #I don't really understand the ARGV[0], just took from read-file.rb demo yesterday, and I don't know when to use it/if I need it here.
+    File.open(ARGV[0]) do |filename|
+      filename.reduce(&:+)
+      filename.gsub(/\n/, ' ')
+    end
   end
 end
+
+
+#I used primarily my code from the code-alongs yesterday and the day before, as well as a bit of the Ruby-Doc documentation for "enumerator" to complete this assessment.
