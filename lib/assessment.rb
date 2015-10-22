bc03696c4a6e9f243a1543d879ef93a79d0aff21
tictacktone <abwtj3@gmail.com>
2015-10-22 10:31:39 -0400

    Almost completed assessment.

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,7 +5,8 @@ class Assessment
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    do enum.max{|a, b| a.length <=> b.length}
+      end
   end
 
   # Question 2
@@ -13,28 +14,29 @@ class Assessment
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+    enum.reduce(:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.odd?
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+   numbers.even?
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized.
   def transform(words)
-    # Your code goes here
+     words.upcase
+     puts words.upcase
   end
 
   # Question 6
@@ -43,6 +45,5 @@ class Assessment
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
   end
 end
