bbfe8ce03941fcc385a2a3dfe94839a9a53b5225
christinemoses <cmoses873@gmail.com>
2015-10-22 10:30:06 -0400

    responses to the ruby enumerables assessment

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,7 +5,8 @@ class Assessment
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    enum.max_by { |x| x.length }
+    # I didn't figure out how to solve this, but was testing ways to use the above line found at http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-max_by
   end
 
   # Question 2
@@ -13,14 +14,18 @@ class Assessment
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+    enum.reduce {0, :+}
+    # I was looking for a solution based on info I found here:
+    # http://stackoverflow.com/questions/1538789/how-to-sum-array-of-numbers-in-ruby
+    # and the enumerable exercise we did with fibonacci in class yesterday
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.select(&:odd?)
+    # I based this answer off of the enumerables exercise we did in class yesterday with fibonacci numbers to find the sum of odd numbers: fibs.select(&:odd?).reduce(&:+), "\n"
   end
 
   # Question 4
