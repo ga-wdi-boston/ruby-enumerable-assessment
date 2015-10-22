b8b06265e3bbdd3c29666c5b597a0231980e8d8a
Lauren Grant <lgrant1286@gmail.com>
2015-10-22 10:31:04 -0400

    Finished assessment

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,37 +5,42 @@ class Assessment
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    enum.max { |a, b| a.length <=> b.length }
   end
 
   # Question 2
   # Write ruby code to return the sum of entries in the
-  # enumerable supplied (i.e. apply the `+` operator)
+  # enumerable supplied
+  #(i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+# What enumerable supplied? Is there something we should actually be working off of?
+  enum.sum #or use the inject method?
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    !numbers.any? &block
   end
+  print numbers
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.all? &block
   end
+  print numbers
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized.
   def transform(words)
-    # Your code goes here
+    words.capitalized
   end
+  print words
 
   # Question 6
   # Write ruby code to read each line from the file
@@ -43,6 +48,8 @@ class Assessment
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
+    file.open("filename.txt") {|f| f.readline}
+    filename.gsub(/\n/, ' ') # replaces newline with a space
+    print filename
   end
 end
