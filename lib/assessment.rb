51fcb3fc882b2588b737f699eb6641a7b1409be9
saphron <saphron.scul@gmail.com>
2015-10-22 10:30:30 -0400

    response

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,7 +5,7 @@ class Assessment
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    enum(&:size).max.last
   end
 
   # Question 2
@@ -13,28 +13,29 @@ class Assessment
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+    enum.reduce(0) do |memo, enum|
+      memo + 1
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    self%1==0 && self.to_i.odd?
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+    self%1==0 && self.to_i.even?
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized.
   def transform(words)
-    # Your code goes here
+    words.map(&:upcase)
   end
 
   # Question 6
