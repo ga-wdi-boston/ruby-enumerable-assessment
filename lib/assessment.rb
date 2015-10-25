748fcf92b20351bb1dc444127393d487f1ca1024
atsundwn <atsundwn@gmail.com>
2015-10-25 08:49:51 -0400

    Re-submitting assessment.

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,7 +5,7 @@ class Assessment
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    enum.reduce { |a, b| a.length <=> b.length }
+    enum.max { |a, b| a.length <=> b.length }
   end
 
   # Question 2
@@ -13,36 +13,28 @@ class Assessment
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    enum.reduce(&:+)
+    enum.reduce(:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    if numbers.each.odd?
-      true
-    else
-      false
-    end
+    numbers.any? { |i| i.odd? }
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    if numbers.each.even? == false
-      false
-    else
-      true
-    end
+    numbers.all? { |i| i.even? }
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized.
   def transform(words)
-    return words.map { |a| a.upcase }
+    words.map { |a| a.capitalize }
   end
 
   # Question 6
@@ -51,8 +43,6 @@ class Assessment
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    text = filename.each.gsub("\n" , " ").join(" ")
-    print text
+    File.open(filename).reduce('') { |memo, line| memo + line.chomp + ' ' }.strip
   end
-
 end
