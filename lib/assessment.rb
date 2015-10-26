bffde2f99610caa4688ddb720c439b3b82d99efc
resource11 <mcmahon@resource11.com>
2015-10-26 00:14:21 -0400

    Second try at assessment

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -4,41 +4,56 @@ class Assessment
   # Write ruby code to return the longest entry in the
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
+
+  # remembering that if there's a parameter passed in
+  # you must add it to whatever method
+  # you're calling
   def longest(enum)
-    max { |a, b| a.length <=> b.length }
+    enum.max { |a, b| a.length <=> b.length }
   end
 
   # Question 2
   # Write ruby code to return the sum of entries in the
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
+
+  # correct syntax here, implicit return statement
   def sum(enum)
-    reduce (memo, f) { return memo + f }
+    enum.reduce (memo, f) { memo + f }
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
 
-  # Yeah... confused
+  # Trying another approach here
+  # using .any? rather than .each
+  # and testing for .odd?
   def some_odd(numbers)
-    each_with_index { |number, index| number.odd? }
+    numbers.any? { |number| number.odd? }
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
 
-  # same comment as above
+  # trying using .all? and .even?
+  # since .all? is like JS .every() method
+  # and only returns true if all elements
+  # are true
   def every_even(numbers)
-    each_with_index { |number, index|  number.even? }
+    numbers.all? { |number, index|  number.even? }
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized.
+
+  # I'm assuming we're receiving an array
+  # of words. Using the .each capitalize the
+  # first letter of each word in place
   def transform(words)
-    upcase
+    words.each { |word| word.capitalize! }
   end
 
   # Question 6
@@ -47,6 +62,11 @@ class Assessment
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
+    File.open(filename) do |file|
+      line_no_newline = file.each_with_object(Hash.new('')) do |line, memo|
+        memo[:lines] += line.gsub!(/"\n/, " ")
+      end
+    end
+    print "The concatenated line is: ", line_no_newline[:lines]
   end
 end
