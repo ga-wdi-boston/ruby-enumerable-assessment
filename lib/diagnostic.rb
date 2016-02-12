bf5cb40fbb26fd22888b1e4356a5234cc28305df
Celena T <chibiynm@gmail.com>
2016-02-12 09:27:45 -0500

    Celena Diagnostic Response

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -5,7 +5,7 @@ class Diagnostic
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    enum.reduce { |memo, obj| memo.length > obj.length ? memo : obj }
   end
 
   # Question 2
@@ -13,28 +13,29 @@ class Diagnostic
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+    enum.reduce(:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.odd?
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+    array = enum.each_with_object([]) { numbers.even? ? true : false }
+    array.include?(false) ? false : true
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
-    # Your code goes here
+    words.each_with_object([]) { words.capitalize! }
   end
 
   # Question 6
@@ -43,6 +44,10 @@ class Diagnostic
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
+    lines = File.open(ARGV[0]) do |file|
+      file.map(&:itself)
+    end
+
+    puts lines
   end
 end
