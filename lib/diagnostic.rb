8f7371d1a574a3add51c40d879dda1411db7d5fb
Ethan <erdesi90562@gmail.com>
2016-02-12 09:28:26 -0500

    Diagnostic complete

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -5,7 +5,7 @@ class Diagnostic
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    enum.length!
   end
 
   # Question 2
@@ -13,21 +13,23 @@ class Diagnostic
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+    enum.each do |prev_item, nex_item|
+      prev_item += nex_item
+    end
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.odd?
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.even?
   end
 
   # Question 5
@@ -35,6 +37,7 @@ class Diagnostic
   # entries from the words enumerable capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
     # Your code goes here
+    words.upcase!
   end
 
   # Question 6
@@ -43,6 +46,10 @@ class Diagnostic
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
+    File.open(filename) do |file|
+    file.each do |line|
+      
+      #replace trailing new line char "\n" with " "
+    end
   end
 end
