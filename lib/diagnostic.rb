482f058b342250b1c4c9754841166228146195c4
Antony Donovan <antony.donovan@generalassemb.ly>
2016-04-14 13:58:38 -0400

    Update prompts
    enumerable -> list

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -1,38 +1,38 @@
 # Ruby enumerable diagnostic
 class Diagnostic
   # Question 1
-  # Write ruby code to return the longest entry in the
-  # enumerable supplied.  Assume each entry implements a length
-  # method that returns a Fixnum.
-  def longest(enum)
+  # Write ruby code to return the longest entry in the list supplied.
+  # Assume each entry in the list implements a length method that returns
+  # a Fixnum.
+  def longest(list)
     # Your code goes here
   end
 
   # Question 2
   # Write ruby code to return the sum of entries in the
-  # enumerable supplied (i.e. apply the `+` operator)
-  # Assume each entry implements a `+` method
-  def sum(enum)
+  # list supplied (i.e. apply the `+` operator).
+  # Assume each entry implements a `+` method.
+  def sum(list)
     # Your code goes here
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
-  # in the numbers enumerable are odd
+  # in the list of numbers are odd
   def some_odd(numbers)
     # Your code goes here
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
-  # numbers enumerable are even
+  # list of numbers are even
   def every_even(numbers)
     # Your code goes here
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
-  # entries from the words enumerable capitalized (i.e. 'foo' becomes 'Foo').
+  # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
     # Your code goes here
   end
