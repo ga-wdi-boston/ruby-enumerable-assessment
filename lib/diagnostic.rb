93873fe199a598447717c015948d91868a016625
gabescarbrough <gabescarbrough@gmail.com>
2016-08-05 09:24:37 -0400

    Complete Ruby Enumerable Diagnostic
    Comfort: 4
    Clarity: 3.5
    
    I referred to the Ruby documentation.
    
    Unsure how to resolve linter warning to pass as argument instead of block, but
    besides that overall comfortable.
    
    Didn't have time to answer last question.

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -5,7 +5,7 @@ class Diagnostic
   # Assume each entry in the list implements a length method that returns
   # a Fixnum.
   def longest(list)
-    # Your code goes here
+    list.max { |a, b| a.length <=> b.length }
   end
 
   # Question 2
@@ -13,28 +13,28 @@ class Diagnostic
   # list supplied (i.e. apply the `+` operator).
   # Assume each entry implements a `+` method.
   def sum(list)
-    # Your code goes here
+    list.reduce(&:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the list of numbers are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.any? { |number| number.odd? }
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # list of numbers are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.all? { |number| number.even? }
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
-    # Your code goes here
+    words.map { |word| word.capitalize }
   end
 
   # Question 6
