a81db5e4b7b3a92d19336f8b970de7f9fe1f6e94
cdelsoin <cdelsoin@gmail.com>
2016-08-05 09:28:38 -0400

    answered ?s

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -5,7 +5,7 @@ class Diagnostic
   # Assume each entry in the list implements a length method that returns
   # a Fixnum.
   def longest(list)
-    # Your code goes here
+    list.sort { |a, b| b.length <=> a.length }
   end
 
   # Question 2
@@ -13,21 +13,21 @@ class Diagnostic
   # list supplied (i.e. apply the `+` operator).
   # Assume each entry implements a `+` method.
   def sum(list)
-    # Your code goes here
+    list.reduce(:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the list of numbers are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.odd?.reduce(:+)
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # list of numbers are even
   def every_even(numbers)
-    # Your code goes here
+     numbers.even? ? true : false
   end
 
   # Question 5
