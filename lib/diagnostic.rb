417d9234e8a3d9536579115843d0284ac9d12a9d
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-09-16 09:27:39 -0400

    Response to diagnostic
    comfort 2
    clarity 2
    
    need to review everything this weekend.

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -5,7 +5,7 @@ class Diagnostic
   # Assume each entry in the list implements a length method that returns
   # a Fixnum.
   def longest(list)
-    # Your code goes here
+    list.max.to_i
   end
 
   # Question 2
@@ -13,28 +13,29 @@ class Diagnostic
   # list supplied (i.e. apply the `+` operator).
   # Assume each entry implements a `+` method.
   def sum(list)
-    # Your code goes here
+    list.reduce(0, :+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the list of numbers are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.each.odd?
+    # NOTE review this after class. Not sure about use of enumerator here.
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # list of numbers are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.values_at(* numbers.each_index.select { |li| li.even? })
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
-    # Your code goes here
+# NOTE review this too..
   end
 
   # Question 6
@@ -43,6 +44,7 @@ class Diagnostic
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
+
+
   end
 end
