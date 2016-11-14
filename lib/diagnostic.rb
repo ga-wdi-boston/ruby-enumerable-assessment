cd65a0afd987c83123895ef73f97d6bf21dbbc41
kmg1434 <kmg1434@gmail.com>
2016-11-14 13:26:19 -0500

    Complete Ruby Enumerable Diagnostic
    respond to prompts in lib/diagnostic.rb

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -6,8 +6,14 @@ class Diagnostic
   # Write ruby code to return the longest entry in the list supplied.
   # Assume each entry in the list implements a length method that returns
   # a Fixnum.
+
   def longest(list)
-    # Your code goes here
+    list.each{ |prev, e|
+      if e.length > prev.length
+        max = e
+      end
+    }
+    max
   end
 
   # Question 2
@@ -15,28 +21,28 @@ class Diagnostic
   # list supplied (i.e. apply the `+` operator).
   # Assume each entry implements a `+` method.
   def sum(list)
-    # Your code goes here
+    list.reduce(:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the list of numbers are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.any?{ |num| num.odd?}
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # list of numbers are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.all?{ |num| num.even?}
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
-    # Your code goes here
+    words.map(&:capitalize)
   end
 
   # Question 6 (optional)
