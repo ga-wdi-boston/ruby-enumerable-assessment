f506fd610ee7be93187214efd0fc6e4f2a7598ce
cpm5041 <christopher.marcenelle@libertymutual.com>
2017-04-14 09:23:08 -0400

    Ruby Enumerable Diagnostic
    Comfort: 3
    Clarity: 3
    
    I understand the concepts of iterating through ruby arrays but
    am still grasping the syntax of certain commands. Also, I still
    struggle with the file-open lesson. Otherwise, I felt good with this.

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -7,7 +7,9 @@ class Diagnostic
   # Assume each entry in the list implements a length method that returns
   # a Fixnum.
   def longest(list)
-    # Your code goes here
+    array = list.split('')
+    array.sort! { |a, b| b.length <=> a.length }
+    array[0]
   end
 
   # Question 2
@@ -15,28 +17,28 @@ class Diagnostic
   # list supplied (i.e. apply the `+` operator).
   # Assume each entry implements a `+` method.
   def sum(list)
-    # Your code goes here
+    list.reduce(:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the list of numbers are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.some?(&:odd)
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # list of numbers are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.all?(&:even)
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
-    # Your code goes here
+    words.map.(&:capitalize)
   end
 
   # Question 6 (optional)
