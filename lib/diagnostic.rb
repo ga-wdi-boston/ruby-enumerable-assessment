d793cf326399b4fb15546a0904d0c91db1d9599d
andyjzhong <andy.zhong@libertymutual.com>
2017-04-14 09:24:13 -0400

    Response
    Comfort: 2
    Clarity: 3
    
    Sources: Google, Ruby Documentation & Notes.
    
    Will review topics over the weekend.

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -7,37 +7,45 @@ class Diagnostic
   # Assume each entry in the list implements a length method that returns
   # a Fixnum.
   def longest(list)
-    # Your code goes here
+    list.each.length.max ...?
   end
+
+# I'll need to review this but I understand we're trying to find the longest word in the list.
+# We would need to iterate through each item in the array and determine the length of each word.
+# Then find and return the maximum number based on the longest word of the list.
 
   # Question 2
   # Write ruby code to return the sum of entries in the
   # list supplied (i.e. apply the `+` operator).
   # Assume each entry implements a `+` method.
   def sum(list)
-    # Your code goes here
+    list.reduce(:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the list of numbers are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.select(&:odd?).any?
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # list of numbers are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.select(&:even?).all?
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
-    # Your code goes here
+    words.map.capitalize
   end
+
+# This seems to only work when capitalizing a string. Here, I would assume we
+# need to use .map in order to come up with a new array before individually
+# capitalizing each word.
 
   # Question 6 (optional)
   # Change `xit` to `it` in `diagnostic_spec.rb` to test this method
