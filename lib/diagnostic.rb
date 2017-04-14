ab72648cf63234dc00ac0810c3b9809ce3109be9
vanduong28 <vanduong28@gmail.com>
2017-04-14 09:28:31 -0400

    Response to ruby enumerable diagnostic
    Response to questions related to ruby enumerable

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -7,7 +7,8 @@ class Diagnostic
   # Assume each entry in the list implements a length method that returns
   # a Fixnum.
   def longest(list)
-    # Your code goes here
+    longest = list.map(&:length).max
+    p longest
   end
 
   # Question 2
@@ -15,28 +16,32 @@ class Diagnostic
   # list supplied (i.e. apply the `+` operator).
   # Assume each entry implements a `+` method.
   def sum(list)
-    # Your code goes here
+    sum = list.inject(:+)
+    p sum
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the list of numbers are odd
   def some_odd(numbers)
-    # Your code goes here
+    some_odd = numbers.any? { |num| num % 2 != 0}
+    p some_odd
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # list of numbers are even
   def every_even(numbers)
-    # Your code goes here
+    every_even = numbers.all? { |num| num % 2 == 0}
+    p every_even
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
   def transform(words)
-    # Your code goes here
+    transform = words.map(&:capitalize)
+    p transform
   end
 
   # Question 6 (optional)
@@ -46,6 +51,7 @@ class Diagnostic
   # replacing the trailing newline character with a
   # space and return the result.
   def read_file(filename)
-    # Your code goes here
+    File.open(filename) do |file|
+      file
   end
 end
